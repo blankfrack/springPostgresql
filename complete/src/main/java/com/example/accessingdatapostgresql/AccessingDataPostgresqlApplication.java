ee78233081608aa185c0fc36ba375af615789e51
blankfrack <blankfrack@users.noreply.github.com>
2020-08-22 16:54:39 -0400

    Update AccessingDataPostgresqlApplication.java

--- complete/src/main/java/com/example/accessingdatapostgresql/AccessingDataPostgresqlApplication.java
+++ complete/src/main/java/com/example/accessingdatapostgresql/AccessingDataPostgresqlApplication.java
@@ -1,4 +1,4 @@
-package com.example.accessingdatapostgrsql;
+package com.example.accessingdatapostgresql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
