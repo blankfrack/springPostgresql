48fdd813b7e2c0b287c11b4ece20a9fedb6a7974
blankfrack <blankfrack@users.noreply.github.com>
2020-08-22 16:54:57 -0400

    Update MainController.java

--- complete/src/main/java/com/example/accessingdatapostgresql/MainController.java
+++ complete/src/main/java/com/example/accessingdatapostgresql/MainController.java
@@ -1,4 +1,4 @@
-package com.example.accessingdatapostgrsql;
+package com.example.accessingdatapostgresql;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
